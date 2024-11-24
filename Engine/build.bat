7838ff308be39dedd6fe761591a478cca54ce129
Evgenii <motleycrue6502@gmail.com>
2024-11-24 20:53:12 +0400

    set upped test proj for windows

--- Engine/build.bat
+++ Engine/build.bat
@@ -2,20 +2,20 @@ REM Build script for engine
 @ECHO OFF
 SetLocal EnableDelayedExpansion
 
-REM Get a list of all the .c files in the engine directory
+REM Get a list of all the .c files.
 SET cFilenames=
-FOR /R . %%F IN (*.c) DO (
-    SET cFilenames=!cFilenames! "%%F"
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
 )
 
 REM echo "Files:" %cFilenames%
 
 SET assembly=engine
-SET compilerFlags= -g -shared -Wvarargs -Wall -Werror
+SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
 REM -Wall -Werror
-SET includeFlags=-Isrc -I%VULCAN_SDK%/Include
-SET linkerFlags=-luser32 -lvulkan-1 -L%VULCAN_SDK%/Lib
-SET defines=-D-DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
+SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
+SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
+SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
 
-ECHO "Building %assembly%..."
+ECHO "Building %assembly%%..."
 clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
